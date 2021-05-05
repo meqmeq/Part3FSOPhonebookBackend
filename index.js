24c81ba9e10bfbf36f3b93cae40720326893dbf0
Marc <mecquilatan@gmail.com>
2021-05-05 09:50:24 -0400

    changed PORT to work with Heroku

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get("/api/persons/:id", (request, response) => {
     return response.status(404).end();
   }
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
